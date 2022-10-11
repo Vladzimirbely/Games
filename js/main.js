b162a5c1e4171c5c6a039dcfc9de2253f1f879cb
VladzimirBely <vladzimir.bely@gmail.com>
2022-10-11 15:42:33 +0300

    create cards

--- js/main.js
+++ js/main.js
@@ -10,3 +10,22 @@ skirt.addEventListener('click', function(evt) {
 difficulty.addEventListener('click', function(evt) {
     btnDifficulty.innerHTML = evt.target.getAttribute('class');
 });
+
+const newGame = document.querySelector('.new-game');
+const gameCards = document.querySelector('.game');
+const descriptionGame = document.querySelector('.description-game');
+
+newGame.addEventListener('click', function(e) {
+    descriptionGame.style.display = 'none';
+    gameCards.style.display = 'flex';
+});
+
+const cards = document.querySelectorAll('.cards');
+const cardCover = document.querySelector('.cards-cover');
+const cardContent = document.querySelector('.cards-content');
+
+cards.forEach((card) => card.addEventListener('click', function(e) {
+    cardCover.style.display = 'none';
+    cardContent.style.display = 'flex';
+}));
+    
